096ee1e97dc4d09bce21be2ede1937f78e12647c
lstellway <lstellway@users.noreply.github.com>
2023-11-19 10:16:04 -0800

    Move git aliases

--- modules/home-manager/shell/aliases.inc.nix
+++ modules/home-manager/shell/aliases.inc.nix
@@ -9,18 +9,6 @@
     "...." = "cd ../../../";
     "....." = "cd ../../../../";
 
-    # Git
-    ga = "git add";
-    gd = "git diff";
-    gpc = "git push origin $(git branch --show-current)";
-    gbn = "printf $(git branch --show-current)";
-
-    # @see https://news.ycombinator.com/item?id=18898523&p=2
-    gitl = "git log --pretty=format:\"%Cred%h %Cgreen %cd %Cblue%<(14,trunc)%an %Creset %s %Cred %d %Creset\" --graph --date=short -20";
-
-    # TODO: Consider
-    # @see https://github.com/paulirish/git-open
-
     # Show ports in use
     ports_tcp = "sudo lsof -iTCP -sTCP:LISTEN -n -P";
     ports_udp = "sudo lsof -iUDP -n -P";
---
+++ modules/home-manager/shell/scripts/git.sh
@@ -0,0 +1,12 @@
+# Git
+alias ga="git add";
+alias gd="git diff";
+alias gpc="git push origin $(git branch --show-current)";
+alias gbn="printf $(git branch --show-current)";
+
+# @see https://news.ycombinator.com/item?id=18898523&p=2
+alias gitl="git log --pretty=format:\"%Cred%h %Cgreen %cd %Cblue%<(14,trunc)%an %Creset %s %Cred %d %Creset\" --graph --date=short -20";
+
+# TODO: Consider
+# @see https://github.com/paulirish/git-open
+
