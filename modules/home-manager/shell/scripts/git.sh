3a8effb7470ed5c7076e2e13c56944613facef8b
lstellway <lstellway@users.noreply.github.com>
2023-12-13 16:28:43 -0800

    Consolidates git shell configuration

--- modules/home-manager/shell/scripts/git.sh
+++ modules/home-manager/shell/scripts/git.sh
@@ -4,7 +4,7 @@ alias gd="git diff";
 alias gpc="git push origin $(git branch --show-current)";
 alias gpo="git pull origin $(git branch --show-current)";
 alias gfo="git fetch origin $(git branch --show-current)";
-alias gbn="printf $(git branch --show-current)";
+alias gbn="git branch --show-current | tee >(pbcopy)";
 
 # @see https://news.ycombinator.com/item?id=18898523&p=2
 alias gitl="git log --pretty=format:\"%Cred%h %Cgreen %cd %Cblue%<(14,trunc)%an %Creset %s %Cred %d %Creset\" --graph --date=short -20";
