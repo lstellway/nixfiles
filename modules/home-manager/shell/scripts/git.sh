345e4c64989a654a3207d676afd7e88b977fa4f4
lstellway <lstellway@users.noreply.github.com>
2023-12-14 18:15:02 -0800

    Single quotes to fix alias creation

--- modules/home-manager/shell/scripts/git.sh
+++ modules/home-manager/shell/scripts/git.sh
@@ -1,10 +1,10 @@
 # Git
-alias ga="git add";
-alias gd="git diff";
-alias gpc="git push origin $(git branch --show-current)";
-alias gpo="git pull origin $(git branch --show-current)";
-alias gfo="git fetch origin $(git branch --show-current)";
-alias gbn="git branch --show-current | tee >(pbcopy)";
+alias ga='git add';
+alias gd='git diff';
+alias gpc='git push origin $(git branch --show-current)';
+alias gpo='git pull origin $(git branch --show-current)';
+alias gfo='git fetch origin $(git branch --show-current)';
+alias gbn='git branch --show-current | tee >(pbcopy)';
 
 # @see https://news.ycombinator.com/item?id=18898523&p=2
 alias gitl="git log --pretty=format:\"%Cred%h %Cgreen %cd %Cblue%<(14,trunc)%an %Creset %s %Cred %d %Creset\" --graph --date=short -20";
