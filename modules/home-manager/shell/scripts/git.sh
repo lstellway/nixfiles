1b23be070886752c50b0917782282cb0603e2454
lstellway <lstellway@users.noreply.github.com>
2024-04-16 17:05:13 -0700

    Remove first line from copied branch text

--- modules/home-manager/shell/scripts/git.sh
+++ modules/home-manager/shell/scripts/git.sh
@@ -5,7 +5,7 @@ alias gw='git worktree';
 alias gpc='git push origin $(git branch --show-current)';
 alias gpo='git pull origin $(git branch --show-current)';
 alias gfo='git fetch origin $(git branch --show-current)';
-alias gbn='git branch --show-current | tee >(pbcopy)';
+alias gbn='git branch --show-current | tee >(tr -d \\n | pbcopy)';
 
 # @see https://news.ycombinator.com/item?id=18898523&p=2
 alias gitl="git log --pretty=format:\"%Cred%h %Cgreen %cd %Cblue%<(14,trunc)%an %Creset %s %Cred %d %Creset\" --graph --date=short -20";
