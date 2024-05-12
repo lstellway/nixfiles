af0cbd3bb0c6cb220385235af41033450e0b8c13
lstellway <lstellway@users.noreply.github.com>
2024-05-11 23:41:00 -0700

    tmux alias

--- modules/home-manager/shell/aliases.inc.nix
+++ modules/home-manager/shell/aliases.inc.nix
@@ -9,6 +9,9 @@
     "...." = "cd ../../../";
     "....." = "cd ../../../../";
 
+    # Tmux
+    tmux-dir = "tmux new -s \"$(basename $(dirname $PWD))/$(basename $PWD)\"";
+
     # Show ports in use
     ports_tcp = "sudo lsof -iTCP -sTCP:LISTEN -n -P";
     ports_udp = "sudo lsof -iUDP -n -P";
