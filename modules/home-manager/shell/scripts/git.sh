7d25db72f3b435073d0a8ef55228289ad5576144
lstellway <lstellway@users.noreply.github.com>
2024-05-11 23:41:18 -0700

    git branch delete alias

--- modules/home-manager/shell/scripts/git.sh
+++ modules/home-manager/shell/scripts/git.sh
@@ -6,6 +6,7 @@ alias gpc='git push origin $(git branch --show-current)';
 alias gpo='git pull origin $(git branch --show-current)';
 alias gfo='git fetch origin $(git branch --show-current)';
 alias gbn='git branch --show-current | tee >(tr -d \\n | pbcopy)';
+alias gbd='git branch -D'
 
 # @see https://news.ycombinator.com/item?id=18898523&p=2
 alias gitl="git log --pretty=format:\"%Cred%h %Cgreen %cd %Cblue%<(14,trunc)%an %Creset %s %Cred %d %Creset\" --graph --date=short -20";
