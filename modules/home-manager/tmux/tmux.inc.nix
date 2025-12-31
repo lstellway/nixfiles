dee444b059604025de4198259e6e90ce9bc710d2
lstellway <lstellway@users.noreply.github.com>
2025-12-31 09:52:16 -0800

    Setup vi mode and yanking

--- modules/home-manager/tmux/tmux.inc.nix
+++ modules/home-manager/tmux/tmux.inc.nix
@@ -2,9 +2,10 @@
   programs.tmux = {
     enable = true;
     clock24 = true;
-    plugins = with pkgs; [
-      tmuxPlugins.sensible
-      tmuxPlugins.vim-tmux-navigator
+    plugins = with pkgs.tmuxPlugins; [
+      sensible
+      vim-tmux-navigator
+      yank
     ];
     extraConfig = pkgs.lib.readFile ./tmux.conf;
   };
